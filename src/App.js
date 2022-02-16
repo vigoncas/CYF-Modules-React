609e332e14faf42195bdbbdc290016101cdc3de6
Vicent González i Castells <vigoncas@programadorphp.es>
2022-02-16 16:46:40 +0100

    Passing Functions as Props

--- src/App.js
+++ src/App.js
@@ -5,9 +5,14 @@ import CaughtPokemon from "./CaughtPokemon";
 function App() {
   const abilities = ["Anticipation", "Adaptability", "Run-Away"];
   const date = new Date().toLocaleDateString();
+
+  function logWhenClicked() {
+    console.log("Image has been clicked");
+  }
+
   return (
     <div>
-      <Logo appName="Pokedex" />
+      <Logo appName="Pokedex" handleClick={logWhenClicked} />
       <BestPokemon abilities={abilities} />
       <CaughtPokemon date={date} />
     </div>
--- src/Logo.js
+++ src/Logo.js
@@ -5,7 +5,7 @@ function Logo(props) {
         <div>
             <header>
                 <h1>Welcome to CYF's {props.appName}</h1>
-                <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" alt=""></img>
+                <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" alt="" onClick={props.handleClick}></img>
             </header>
         </div>
     );
